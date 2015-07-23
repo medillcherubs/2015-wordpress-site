009712f2171cb58840600b55f730d1e40dbb0a2f
Tom Giratikanon <giratikanon@gmail.com>
2015-07-23 17:24:39 -0500

    Remove favicons

--- wp-content/themes/cherubs-2015/header.php
+++ wp-content/themes/cherubs-2015/header.php
@@ -9,15 +9,6 @@
   <title><?php wp_title('', true, 'right'); ?> - <?php echo get_bloginfo("name") ?></title>
 
   <!-- icons & favicons -->
-  <!-- For iPhone 4 -->
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/h/apple-touch-icon.png">
-  <!-- For iPad 1-->
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/m/apple-touch-icon.png">
-  <!-- For iPhone 3G, iPod Touch and Android -->
-  <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/l/apple-touch-icon-precomposed.png">
-  <!-- For Nokia -->
-  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/l/apple-touch-icon.png">
-  <!-- For everything else -->
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
 
   <!-- media-queries.js (fallback) -->
@@ -47,6 +38,7 @@
 
     <!-- HEADER -->
     <div class="twelve columns">
+
       <!-- MAIN LOGO -->
       <header role="banner" class="logo-header">
         <a class="logo" href="<?php echo get_bloginfo('url'); ?>">
@@ -54,14 +46,13 @@
         </a>
       </header>
 
-      <!-- MENU -->
+      <!-- TOP MENU -->
       <?php wp_nav_menu( array(
           "theme_location" => "header-menu",
           "menu_class" => "navigation-menu"
         ) ); ?>
 
     </div> <!-- HEADER -->
-    <?php // bones_main_nav(); // Adjust using Menus in Wordpress Admin ?>
 
     <!-- CONTENT -->
     <div class="content twelve columns">
